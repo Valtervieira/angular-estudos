e3cee2d891edb39d14aa9efc5ac345918da9a7ec
Valter <valtervieira123@gmail.com>
2018-01-29 21:14:24 -0200

    Desvendo o Javascript

---
+++ Documents/angular-estudos/date.js
@@ -0,0 +1,26 @@
+var hoje = new Date();
+
+console.log(hoje);
+
+/*Exemplo*/
+date.parse("2014/12/25")
+
+date.parse("2014-12-25")
+
+
+/*Forma correta de tempo no Brasil segundo a norma ISO*/
+new Date("2014-12-25T10:30-02:00")
+
+
+
+
+
+/*
+obs: passar sempre no parse como ano/mes/dia 
+
+
+lembrar sempre da timezone que tera impacto 
+sobre o tempo do meridiano 
+
+
+*/
